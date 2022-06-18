75444d1d9684e0ea1b56c98e2f926eebfa12f9bb
Supanut1911 <ynotmela@gmail.com>
2022-06-18 11:17:05 +0700

    add-paragraph-and-add-lib-react-router-dom

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,12 @@ const Home: NextPage = () => {
   return (
     <>
       <h1 style={{ color: "red" }}>Hi gitflow</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+        laboriosam reiciendis at molestiae nisi porro eum iusto id, architecto
+        asperiores, distinctio, totam aspernatur ab inventore. Consequatur iste
+        voluptates nesciunt illo.
+      </p>
     </>
   );
 };
